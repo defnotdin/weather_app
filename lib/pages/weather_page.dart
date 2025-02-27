a37dfe505affbef3ed1205927fa4596eedde1081
adina <adin@adinas-MacBook-Air.local>
2025-02-27 13:05:02 +0500

    first commit

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/weather_model.dart';
+import 'package:flutter_application_1/services/weather_service.dart';
+import 'package:lottie/lottie.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('b171f6de8cf591cf1a6bc70c3e21dec8');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rainy.json';
+      case 'thunderstorm':
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "loading city..."),
+
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+            Text('${_weather?.temperature.round()}C'),
+
+            Text(_weather?.mainCondition ?? ""),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/weather_service.dart
@@ -0,0 +1,48 @@
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../models/weather_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WeatherService {
+  static const String BASE_URL =
+      "https://api.openweathermap.org/data/3.0/onecall";
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http.get(
+      Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=Taraz,KZ&appid=b171f6de8cf591cf1a6bc70c3e21dec8&units=metric",
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    List<Placemark> placemark = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+
+    String? city = placemark[0].locality;
+
+    return city ?? "";
+  }
+}
